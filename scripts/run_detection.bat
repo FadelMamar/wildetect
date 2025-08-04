147071b0ae3d7cc4386ddcc1ade81d6245f3dd27
fadel <fadel.seydou@gmail.com>
2025-08-04 11:27:08 +0200

    update

--- scripts/run_detection.bat
+++ scripts/run_detection.bat
@@ -3,9 +3,19 @@ call .venv\Scripts\activate
 
 @REM --roi-weights "D:\PhD\workspace\wildetect\models\classifier\2\artifacts\best.ckpt-v6.torchscript"^
 
-call wildetect detect "D:\PhD\Data per camp\tmp"^
-  --model "D:\PhD\workspace\wildetect\models\labeler\9\artifacts\best.pt"^
+call wildetect detect "D:\workspace\data\savmap_dataset_v2\raw\images"^
+  --model "D:\workspace\repos\wildetect\weights\best.pt"^
   --output "results"^
-  --device "auto" 
+  --device "auto"^
+  --inference-service-url "http://localhost:4141/predict"^
+  --pipeline-type "single" --queue-size 10 ^
+  --overlap-ratio 0.2^
+  --batch-size 32^
+  --tile-size 800^
+  --cls-imgsz 128^
+  --sensor-height 24.0^
+  --focal-length 35.0^
+  --flight-height 180.0^
+
   
 @REM --profile --gpu-profile
