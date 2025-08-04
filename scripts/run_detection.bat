251d39919cbdfff6e906cfe680ef1fd9b9331051
fadel <fadel.seydou@gmail.com>
2025-08-04 12:17:55 +0200

    update

--- scripts/run_detection.bat
+++ scripts/run_detection.bat
@@ -3,9 +3,11 @@ call .venv\Scripts\activate
 
 @REM --roi-weights "D:\PhD\workspace\wildetect\models\classifier\2\artifacts\best.ckpt-v6.torchscript"^
 
-call wildetect detect "D:\PhD\Data per camp\tmp"^
+call wildetect detect "D:\PhD\Data per camp\Dry season\Kapiri\Farm\DJI_202310040946_001_KapiriFarm1"^
   --model "D:\PhD\workspace\wildetect\models\labeler\9\artifacts\best.pt"^
-  --output "results"^
-  --device "auto" 
-  
+  --roi-weights "D:\PhD\workspace\wildetect\models\classifier\2\artifacts\best.ckpt-v6.torchscript"^
+  --device "auto"
+
+call pause
+
 @REM --profile --gpu-profile
