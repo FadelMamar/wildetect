c44062bcc45eedb41666d1de7ccf025936d5b4f5
fadel <fadle>
2025-08-11 12:59:02 +0200

    update

--- scripts/launch_inference_server.bat
+++ scripts/launch_inference_server.bat
@@ -1,6 +1,6 @@
 call cd /d %~dp0 
 call cd ..
 
-call uv run wildetect inference-server --port 4141 --workers 2
+call uv run wildetect services inference-server --port 4141 --workers 2
 
 call pause
