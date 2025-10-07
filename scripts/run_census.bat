167dc3ed39161b173bb023c1f7bcf2aede92e948
fadel <fadel.seydou@gmail.com>
2025-10-07 20:08:27 +0200

    update batch scripts

--- scripts/run_census.bat
+++ scripts/run_census.bat
@@ -1,6 +1,16 @@
 call cd /d "%~dp0" && cd ..
 call .venv\Scripts\activate
 
-call uv run wildetect detection census -c config/census.yaml
+@REM Do not forget to launch Label Studio and Fiftyone if you want to visualize the results
+
+if exist .env (
+    call .\scripts\load_env.bat
+    call uv run wildetect detection census -c config/census.yaml
+    pause
+
+) else (
+    echo .env file not found. Please provide it in the root directory.
+    pause
+)
 
 call pause
--- scripts/run_detection.bat
+++ scripts/run_detection.bat
@@ -1,9 +1,9 @@
 call cd /d "%~dp0" && cd ..
 
+@REM Do not forget to launch Fiftyone if you want to visualize the results
 
 call uv run wildetect detection detect  -c config/detection.yaml
 
 call pause
-
   
 @REM --profile --gpu-profile
