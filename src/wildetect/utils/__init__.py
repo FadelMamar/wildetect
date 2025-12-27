7fbf2652bbe7fa0859bb5ae79d50fced5189b5f4
fadel <fadel.seydou@gmail.com>
2025-12-27 19:07:24 +0100

    update

--- src/wildetect/utils/__init__.py
+++ src/wildetect/utils/__init__.py
@@ -3,6 +3,7 @@ Utility functions for WildDetect.
 
 This package contains configuration, image processing, and other utility functions.
 """
+from .benchmark import IoUTuner
 from .profiler import profile_command
 from .utils import compute_iou, get_experiment_id
 
@@ -10,4 +11,5 @@ __all__ = [
     "profile_command",
     "compute_iou",
     "get_experiment_id",
+    "IoUTuner",
 ]
