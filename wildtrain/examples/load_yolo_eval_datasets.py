b04ba3c768f9691ca5cdc88e3c4fe2a3034c94ce
fadel <fadel.seydou@gmail.com>
2025-12-28 22:50:00 +0200

    update

--- wildtrain/examples/load_yolo_eval_datasets.py
+++ wildtrain/examples/load_yolo_eval_datasets.py
@@ -1,9 +1,8 @@
 from wildtrain.utils.io import load_all_detection_datasets
 
-def main(root_data_directory:str,split:str):
+def main(root_data_directory:str=r"D:\PhD\workspace\data",split:str="val"):
     datasets = load_all_detection_datasets(root_data_directory=root_data_directory, split=split)
     print(f"Loaded {len(datasets)} datasets for split {split}")
 
 if __name__ == "__main__":
-    import fire
-    fire.Fire(main)
+    main()
