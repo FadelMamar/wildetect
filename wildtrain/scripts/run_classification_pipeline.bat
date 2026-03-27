cfaed9afcc14ed0b203bb75a94e8159d426cf882
fadel <fadel.seydou@gmail.com>
2026-03-27 19:56:08 +0200

    fix: update CONFIG_PATH and CONFIG_FILE in batch scripts to use relative paths

--- wildtrain/scripts/run_classification_pipeline.bat
+++ wildtrain/scripts/run_classification_pipeline.bat
@@ -3,7 +3,7 @@ REM Example script to run the classification pipeline using WildTrain CLI
 call cd /d %~dp0 && cd ..
 
 REM Set example config path
-set CONFIG_PATH=wildtrain\configs\classification\classification_pipeline_config.yaml
+set CONFIG_PATH=configs\classification\classification_pipeline_config.yaml
 
 REM Run the run_classification_pipeline CLI command
 call uv run wildtrain pipeline classification ^
--- wildtrain/scripts/train_classifier.bat
+++ wildtrain/scripts/train_classifier.bat
@@ -1,5 +1,5 @@
 call cd /d %~dp0 && cd ..
 
-call set CONFIG_FILE=wildtrain\configs\classification\classification_train.yaml
+call set CONFIG_FILE=configs\classification\classification_train.yaml
 
 call uv run wildtrain train classifier -c %CONFIG_FILE%
