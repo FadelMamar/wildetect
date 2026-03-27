8e34edef7c001919d3a07999a94e54e97d426403
fadel <fadel.seydou@gmail.com>
2026-03-27 19:58:44 +0200

    fix: update CONFIG_PATH in eval_classifier.bat to use relative path

--- wildtrain/scripts/eval_classifier.bat
+++ wildtrain/scripts/eval_classifier.bat
@@ -4,7 +4,7 @@ REM Example script to evaluate a classifier using WildTrain CLI
 call cd /d "%~dp0" && cd ..
 
 REM Set example config path
-set CONFIG_PATH=wildtrain\configs\classification\classification_eval.yaml
+set CONFIG_PATH=configs\classification\classification_eval.yaml
 
 REM Run the evaluate-classifier CLI command
 call uv run wildtrain evaluate classifier -c %CONFIG_PATH% 
